2b9cb71a4ec70d0d17f1c9deb63d0fbe12bcaae4
NebzHB <nebz@nebz.be>
2017-08-24 16:31:15 +0200

    NodeJS 6 is LTS and should be allowed

--- resources/nodejs.sh
+++ resources/nodejs.sh
@@ -14,7 +14,7 @@ echo 10 > /tmp/espeasy_dep
 actual=`nodejs -v`;
 echo "Version actuelle : ${actual}"
 
-if [[ $actual == *"4."* || $actual == *"5."* ]]
+if [[ $actual == *"4."* || $actual == *"5."* || $actual == *"6."* ]]
 then
   echo "Ok, version suffisante";
 else
