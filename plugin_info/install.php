268064783d9560ade40fee305457ec8d465143c1
Cédric Dupont <lunarok@gmail.com>
2018-03-16 18:01:52 +0100

    Create install.php

---
+++ plugin_info/install.php
@@ -0,0 +1,27 @@
+<?php
+
+/* This file is part of Jeedom.
+ *
+ * Jeedom is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Jeedom is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Jeedom. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+require_once dirname(__FILE__) . '/../../../core/php/core.inc.php';
+
+function espeasy_update() {
+    espeasy::deamon_stop();
+    $sensor_path = dirname(__FILE__) . '/../resources';
+    exec('cd ' . $sensor_path . '; rm -rf node_modules');
+}
+
+?>
